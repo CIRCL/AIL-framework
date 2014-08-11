5bc0f154f99efdc59433321c762a589b69fa75fe
Starow <le.bassiste.575@gmail.com>
2014-08-11 09:36:28 +0200

    Update of installing_deps.sh and pip_requirement.txt

--- installing_deps.sh
+++ installing_deps.sh
@@ -9,12 +9,55 @@ sudo apt-get install python-pip
 sudo apt-get install python-virtualenv
 sudo apt-get install python-dev
 sudo apt-get install libfreetype6-dev
-
+sudo apt-get install screen
 
 virtualenv AILENV
 
+echo export AIL_HOME=$(pwd) >> ./AILENV/bin/activate
+echo export AIL_BIN=$(pwd)/bin/ >> ./AILENV/bin/activate
+echo export AIL_FLASK=$(pwd)/var/www/ >> ./AILENV/bin/activate
+
+sudo apt-get install g++
+sudo apt-get install python-tk
+
+#Needed for bloom filters
+sudo apt-get install libssl-dev
+
+sudo apt-get install libfreetype6-dev
+sudo apt-get install python-numpy
+
+#needed for mathplotlib
+test ! -L /usr/include/ft2build.h && sudo ln -s freetype2/ft2build.h /usr/include/
+
 . ./AILENV/bin/activate
 
-pip install -r requirements.txt --upgrade
+pip install -r pip_packages_requirement.txt --upgrade
 
+pip install -U textblob
 python -m textblob.download_corpora
+
+# REDIS #
+test ! -d redis/ && git clone https://github.com/antirez/redis.git
+cd redis/
+git checkout 2.8
+git pull
+make
+
+echo export AIL_REDIS = $(pwd)/src/ >> ./AILENV/bin/activate
+
+# REDIS LEVEL DB #
+cd $AIL_HOME
+test ! -d redis-leveldb/ && git clone https://github.com/KDr2/redis-leveldb.git
+cd redis-leveldb/
+git submodule init
+git submodule update
+
+cd $AIL_HOME
+mkdir -p PASTES
+mkdir -p Blooms
+mkdir -p dumps
+
+mkdir -p LEVEL_DB_DATA
+cd LEVEL_DB_DATA
+mkdir -p 2014
+mkdir -p 2013
