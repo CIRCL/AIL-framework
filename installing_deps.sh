3f066ddaec509c86a157e8a57fd2d0771e21cc06
Raphaël Vinot <raphael@vinot.info>
2014-08-18 13:40:07 +0200

    fix install script

--- installing_deps.sh
+++ installing_deps.sh
@@ -5,11 +5,7 @@ set -x
 
 sudo apt-get update
 
-sudo apt-get install python-pip
-sudo apt-get install python-virtualenv
-sudo apt-get install python-dev
-sudo apt-get install libfreetype6-dev
-sudo apt-get install screen
+sudo apt-get install python-pip python-virtualenv python-dev libfreetype6-dev screen
 
 virtualenv AILENV
 
@@ -17,14 +13,13 @@ echo export AIL_HOME=$(pwd) >> ./AILENV/bin/activate
 echo export AIL_BIN=$(pwd)/bin/ >> ./AILENV/bin/activate
 echo export AIL_FLASK=$(pwd)/var/www/ >> ./AILENV/bin/activate
 
-sudo apt-get install g++
-sudo apt-get install python-tk
+sudo apt-get install g++ python-tk
 
 #Needed for bloom filters
-sudo apt-get install libssl-dev
+sudo apt-get install libssl-dev libfreetype6-dev python-numpy
 
-sudo apt-get install libfreetype6-dev
-sudo apt-get install python-numpy
+# DNS deps
+sudo apt-get install libadns1 libadns1-dev
 
 #needed for mathplotlib
 test ! -L /usr/include/ft2build.h && sudo ln -s freetype2/ft2build.h /usr/include/
@@ -38,32 +33,22 @@ python -m textblob.download_corpora
 
 # REDIS #
 test ! -d redis/ && git clone https://github.com/antirez/redis.git
-cd redis/
+pushd redis/
 git checkout 2.8
 git pull
 make
+popd
 
-echo export AIL_REDIS = $(pwd)/src/ >> ./AILENV/bin/activate
+echo export AIL_REDIS=$(pwd)/src/ >> ./AILENV/bin/activate
 
 # REDIS LEVEL DB #
-cd $AIL_HOME
 test ! -d redis-leveldb/ && git clone https://github.com/KDr2/redis-leveldb.git
-cd redis-leveldb/
+pushd redis-leveldb/
 git submodule init
 git submodule update
+popd
 
-cd $AIL_HOME
-mkdir -p PASTES
-mkdir -p Blooms
-mkdir -p dumps
+mkdir -p $AIL_HOME/{PASTES,Blooms,dumps}
 
-mkdir -p LEVEL_DB_DATA
-cd LEVEL_DB_DATA
-mkdir -p 2014
-mkdir -p 2013
-
-cd $AIL_HOME
-test ! -d langid.py/ && git clone https://github.com/saffsd/langid.py.git
-cd langid.py/
-python setup.py install
+mkdir -p $AIL_HOME/LEVEL_DB_DATA/{2014,2013}
 
