06d6d8f286b02c6a524fa9fade1e569c18205b4f
Alainfou <fou.alain@gmail.com>
2016-03-14 10:20:59 -0400

    More coherent and commented module. Still some debug lines and not yet optimized, though.

--- bin/Ransom.py
+++ bin/Ransom.py
@@ -2,8 +2,19 @@
 # -*-coding:UTF-8 -*
 """
 	Module for specific text analysis
-	More global aim is to detect an attempt of: Ransom, Phishing, Social Engineering, etc.
-	Here, only RANSOM is sought for now, and on every Paste.
+	More global aim would be to detect an attempt of: Ransom, Phishing, Social Engineering, etc.
+	Here, only ENGLISH words, and related to Ransoms/Ransomwares are sought for now.
+	When this module will run smoothly and give results, it'll be a good framework for other threatening Pastes (phishing, dox, skids, etc.)
+
+	Module's format:
+	- analyse whether the content is in natural human language ( <-- ignored, we actually check every Paste)
+	- a file "AIL-framework/nltk_data/corpora/dict/ransom.dic" lists keywords and their (arbitrary) weight
+	- keep a local var dict_ransom["word": weight]
+	- tested texts are lowercased
+	- compare the text and look for the keywords from "dict_ransom"
+	- add the weight of those keywords in a "counter" variable (representing the probability of an actual threatining ransom)
+	- ignore repetitions (otherwise, "database" would increase N times the counter, even if it has a weight of 1 and is barely relevant)
+	- raise an alert only if the threat of an actual ransom message is higher than an (arbitrary) threshold
 """
 
 import time
@@ -13,21 +24,10 @@ from packages import lib_refine
 from pubsublogger import publisher
 from Helper import Process
 
-"""
-	Forme du module :
-	- analyser si ca contient du texte en langage naturel
-	- Lower-Case les textes testes
-	- ouvrir le fichier "dictionnaire", garder le contenu dans une variable dict["mot": poids] en memoire locale
-	- analyser les mots significatifs
-	- ignorer les repetitions (sinon, "database" augmenterait N fois le taux de menace, alors qu'il a un poids de seulement 1)
-	- faire une somme des indices de "menaces"
-	- ne lever une alerte qu'en cas de menace superieure a un seuil (arbitraire)
-"""
-
-# Dictionnaire contenant les mots a chercher (indicatifs de RANSOMWARE seulement)
+# Dictionary containing the words to look for, with an arbitrary weight (only for Ransoms and Ransomwares)
 dict_ransom = {}
 
-# Fonction de recherche de mots-cles de rancons, encore peu optimisee
+# Function looking for keywords related to a Ransom or Ransomware in a given Paste
 def search_ransom(message):
 	counter = 0
 	str_DEBUG = ''
@@ -35,23 +35,27 @@ def search_ransom(message):
 	content = paste.get_p_content()
 	for word in content.split():
 		word = word.lower()
+		#if a keyword is found, we add its weight to the total counter
 		if word in dict_ransom:
 			counter += dict_ransom[word]
 
 			#for debugging! Delete ASAP
 			str_DEBUG += (word+'\t')
-			print counter
+	#for debugging! Delete ASAP
 	print str_DEBUG
+	#for debugging! Delete ASAP
 	print counter
 
-	# if the list is greater than 4, we consider the Paste may contain a list of phone numbers
-	if counter > 10 :
-		# FOR DEBBUGGUNG ! DELETE ASAP !!
+	# if the sum of threat indices is greater than 42, we consider that the Paste may be related to a Ransom or Ransomware
+	if counter > 42 :
+
+		#for debugging! Delete ASAP
 		print str_DEBUG
+
 		publisher.warning('{} may be a Ransom!'.format(paste.p_name))
 	return None
 
-# fonction principale du module
+# Module's main function
 
 if __name__ == '__main__':
 	# If you wish to use an other port of channel, do not forget to run a subscriber accordingly (see launch_logs.sh)
@@ -70,10 +74,10 @@ if __name__ == '__main__':
 	publisher.info("Run Ransom module")
 
 	# Getting the dictionary from a file to the memory
-	with open('nltk_data/corpora/dict/ransom.dic') as file:
-		for line in file:
+	with open('nltk_data/corpora/dict/ransom.dic') as dict_file:
+		for line in dict_file:
 			k, v = line.split()
-			dict_ransom[k] = v
+			dict_ransom[k] = int(v)
 
 	# Endless loop getting messages from the input queue
 	while True:
