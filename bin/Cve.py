beeeb76de93f97ccf86e9f5546a6e3070b2bcfd0
Mokaddem <mokaddem.sami@gmail.com>
2016-06-30 14:38:28 +0200

    Added new modules and started WebTrending web interface

---
+++ bin/Cve.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+# -*-coding:UTF-8 -*
+"""
+    Template for new modules
+"""
+
+import time
+import re
+from pubsublogger import publisher
+from packages import Paste
+from Helper import Process
+
+
+def search_cve(message):
+    filepath, count = message.split()
+    paste = Paste.Paste(filepath)
+    content = paste.get_p_content()
+    # regex to find CVE
+    reg_cve = re.compile(r'(CVE-)[1-2]\d{1,4}-\d{1,5}')
+    # list of the regex results in the Paste, may be null
+    results = set(reg_cve.findall(content))
+
+    # if the list is greater than 2, we consider the Paste may contain a list of cve
+    if len(results) > 0:
+        print('{} contains CVEs'.format(paste.p_name))
+        publisher.warning('{} contains CVEs'.format(paste.p_name))
+
+if __name__ == '__main__':
+    # If you wish to use an other port of channel, do not forget to run a subscriber accordingly (see launch_logs.sh)
+    # Port of the redis instance used by pubsublogger
+    publisher.port = 6380
+    # Script is the default channel used for the modules.
+    publisher.channel = 'Script'
+
+    # Section name in bin/packages/modules.cfg
+    config_section = 'Cve'
+
+    # Setup the I/O queues
+    p = Process(config_section)
+
+    # Sent to the logging a description of the module
+    publisher.info("Run CVE module")
+
+    # Endless loop getting messages from the input queue
+    while True:
+        # Get one message from the input queue
+        message = p.get_from_set()
+        if message is None:
+            publisher.debug("{} queue is empty, waiting".format(config_section))
+            time.sleep(1)
+            continue
+
+        # Do something with the message from the queue
+        search_cve(message)
+
+        # (Optional) Send that thing to the next queue
+        #p.populate_set_out(something_has_been_done)
---
+++ bin/empty_queue.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+# -*-coding:UTF-8 -*
+
+"""
+The Duplicate module
+====================
+
+This huge module is, in short term, checking duplicates.
+
+Requirements:
+-------------
+
+
+"""
+import redis
+import os
+import time
+from packages import Paste
+from pubsublogger import publisher
+from Helper import Process
+
+if __name__ == "__main__":
+    publisher.port = 6380
+    publisher.channel = "Script"
+
+    config_section = ['Global', 'Duplicates', 'Indexer', 'Attributes', 'Lines', 'DomClassifier', 'Tokenize', 'Curve', 'Categ', 'CreditCards', 'Mail', 'Onion', 'DumpValidOnion', 'Web',  'WebStats', 'Release', 'Credential', 'Cve', 'Phone', 'SourceCode', 'Keys']
+
+    for queue in config_section:
+        print 'dropping: ' + queue
+        p = Process(queue)
+        while True:
+            message = p.get_from_set()
+            if message is None:
+                break
+
