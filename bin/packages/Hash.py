0332f23579ae7dee01c7f42db72aff070c8aa019
Mokaddem <mokaddem.sami@gmail.com>
2016-07-15 08:56:16 +0200

    Added SimHash library

--- bin/packages/Hash.py
+++ bin/packages/Hash.py
@@ -1,6 +1,7 @@
 import hashlib
 import crcmod
 import mmh3
+import simhash
 
 
 class Hash(object):
@@ -31,5 +32,8 @@ class Hash(object):
 
         elif self.name == "murmur":
             hash = mmh3.hash(string)
+
+        elif self.name == "simhash":
+            hash = Simhash(string)
 
         return hash
