2c41e13cb60541e2915d9454d95bf556fe5fbb83
Mokaddem <mokaddem.sami@gmail.com>
2017-02-28 10:50:00 +0100

    Updated archlinux instaler

--- installing_deps_archlinux.sh
+++ installing_deps_archlinux.sh
@@ -37,9 +37,36 @@ git submodule update
 make
 popd
 
+# Faup
+test ! -d faup/ && git clone https://github.com/stricaud/faup.git
+pushd faup/
+test ! -d build && mkdir build
+cd build
+cmake .. && make
+sudo make install
+echo '/usr/local/lib' | sudo tee -a /etc/ld.so.conf.d/faup.conf
+sudo ldconfig
+popd
+
+# tlsh
+test ! -d tlsh && git clone git://github.com/trendmicro/tlsh.git
+pushd tlsh/
+./make.sh
+pushd build/release/
+sudo make install
+sudo ldconfig
+popd
+popd
+
+
+
 if [ ! -f bin/packages/config.cfg ]; then
     cp bin/packages/config.cfg.sample bin/packages/config.cfg
 fi
+
+pushd var/www/
+./update_thirdparty.sh
+popd
 
 virtualenv AILENV
 
@@ -52,9 +79,26 @@ echo export AIL_LEVELDB=$(pwd)/redis-leveldb/ >> ./AILENV/bin/activate
 . ./AILENV/bin/activate
 
 mkdir -p $AIL_HOME/{PASTES,Blooms,dumps}
-mkdir -p $AIL_HOME/LEVEL_DB_DATA/{2014,2013}
+mkdir -p $AIL_HOME/LEVEL_DB_DATA/2017
+mkdir -p $AIL_HOME/LEVEL_DB_DATA/3017
 
-pip install -r pip_packages_requirement.txt
+pip install -U pip
+pip install -U -r pip_packages_requirement.txt
 
-# Download the necessary NLTK corpora
+# Pyfaup
+pushd faup/src/lib/bindings/python/
+python setup.py install
+popd
+
+# Py tlsh
+pushd tlsh/py_ext
+python setup.py build
+python setup.py install
+
+# Download the necessary NLTK corpora and sentiment vader
 HOME=$(pwd) python -m textblob.download_corpora
+python -m nltk.downloader vader_lexicon
+python -m nltk.downloader punkt
+
+#Create the file all_module and update the graph in doc
+$AIL_HOME/doc/generate_modules_data_flow_graph.sh
