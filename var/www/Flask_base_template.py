71b2757ed20addbdaf6717ff9fd73389fbe811b2
Mokaddem <mokaddem.sami@gmail.com>
2017-04-25 15:22:04 +0200

    New script create_new_web_module permits to rapidly create a new pre-configured webpage from a template.

---
+++ var/www/Flask_base_template.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*-coding:UTF-8 -*
+
+'''
+    Flask functions and routes for the trending modules page
+'''
+import redis
+from flask import Flask, render_template, jsonify, request, Blueprint
+
+# ============ VARIABLES ============
+import Flask_config
+
+app = Flask_config.app
+cfg = Flask_config.cfg
+
+MODULENAME = Blueprint('MODULENAME', __name__, template_folder='templates')
+
+# ============ FUNCTIONS ============
+def one():
+    return 1
+
+# ============= ROUTES ==============
+
+@MODULENAME.route("/MODULENAME/", methods=['GET'])
+def MODULENAME_page():
+    return render_template("MODULENAME.html")
+
+
+# ========= REGISTRATION =========
+app.register_blueprint(MODULENAME)
---
+++ var/www/create_new_web_module.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*-coding:UTF-8 -*
+
+"Hepler to create a new webpage associated with a module."
+
+import os
+
+def createModuleFolder(modulename):
+    path_module = os.path.join('modules', modulename)
+    os.mkdir(path_module)
+    
+    # create html template
+    with open('templates/base_template.html', 'r') as templateFile:
+        template = templateFile.read()
+        template = template.replace('MODULENAME', modulename)
+    
+    os.mkdir(os.path.join(path_module, 'templates'))
+    with open(os.path.join(os.path.join(path_module, 'templates'), modulename+'.html'), 'w') as toWriteTemplate:
+        toWriteTemplate.write(template)
+
+    # create html header template
+    with open('templates/header_base_template.html', 'r') as header_templateFile:
+        header = header_templateFile.read()
+        header = header.replace('MODULENAME', modulename)
+    
+    with open(os.path.join(os.path.join(path_module, 'templates'), 'header_{}.html'.format(modulename) ), 'w') as toWriteHeader:
+        toWriteHeader.write(header)
+
+
+    #create flask template
+    with open('Flask_base_template.py', 'r') as flaskFile:
+        flask = flaskFile.read()
+        flask = flask.replace('MODULENAME', modulename)
+
+    with open(os.path.join(path_module, 'Flask_{}.py'.format(modulename)), 'w') as toWriteFlask:
+        toWriteFlask.write(flask)
+
+
+def main():
+	rep1 = raw_input('New module name: ')
+	createModuleFolder(rep1)
+
+if __name__ == '__main__':
+	main()
