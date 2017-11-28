71922f9def5c635942d9f32ad5576f582935042d
Alexandre Dulaunoy <a@foo.be>
2017-11-28 09:46:06 +0100

    add: Python script to test if the ZMQ feed works as expected

---
+++ bin/feeder/test-zmq.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of AIL framework - Analysis Information Leak framework
+#
+#
+# Python script to test if the ZMQ feed works as expected
+#
+
+import sys
+import zmq
+
+port = "5556"
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect ("tcp://crf.circl.lu:%s" % port)
+
+# 101 Name of the pastes only
+# 102 Full pastes in raw base64(gz)
+
+topicfilter = "102"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+while True:
+    message = socket.recv()
+    print (message)
+    if topicfilter == "102":
+        topic, paste, messagedata = message.split()
+        print paste, messagedata
+    else:
+        print (message)
