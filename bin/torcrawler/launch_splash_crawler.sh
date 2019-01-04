b3b75ccbeac8615eef720358299f5de959af4b07
Terrtia <or1994@hotmail.fr>
2019-01-04 15:51:08 +0100

    fix: [Crawler] Restart Splash on failure, limit unbound in memory cache (maxrss)

--- bin/torcrawler/launch_splash_crawler.sh
+++ bin/torcrawler/launch_splash_crawler.sh
@@ -5,12 +5,15 @@ usage() { echo "Usage: sudo $0 [-f <config_absolute_path>] [-p <port_start>] [-n
           echo "          -p: number of the first splash server port number. This number is incremented for the others splash server";
           echo "          -n: number of splash servers to start";
           echo "";
+          echo "          -options:";
+          echo "          -u: max unbound in-memory cache (Mb, Restart Splash when full, default=3000 Mb)";
+          echo "";
           echo "example:";
           echo "sudo ./launch_splash_crawler.sh -f /home/my_user/AIL-framework/configs/docker/splash_onion/etc/splash/proxy-profiles/ -p 8050 -n 3";
           exit 1;
         }
 
-while getopts ":p:f:n:" o; do
+while getopts ":p:f:n:u:" o; do
     case "${o}" in
         p)
             p=${OPTARG}
@@ -21,12 +24,19 @@ while getopts ":p:f:n:" o; do
         n)
             n=${OPTARG}
             ;;
+        u)
+            u=${OPTARG}
+            ;;
         *)
             usage
             ;;
     esac
 done
 shift $((OPTIND-1))
+
+if [ -z "${u}" ]; then
+    u=3000;
+fi
 
 if [ -z "${p}" ] || [ -z "${f}" ] || [ -z "${n}" ]; then
     usage;
@@ -52,7 +62,7 @@ sleep 0.1
 
 for ((i=0;i<=$((${n} - 1));i++)); do
     port_number=$((${p} + $i))
-    screen -S "Docker_Splash" -X screen -t "docker_splash:$port_number" bash -c 'sudo docker run -p '$port_number':8050 --cpus=1 --memory=4.5G -v '$f':/etc/splash/proxy-profiles/ --net="bridge" scrapinghub/splash; read x'
+    screen -S "Docker_Splash" -X screen -t "docker_splash:$port_number" bash -c 'sudo docker run -d -p '$port_number':8050 --restart=always --cpus=1 --memory=4.5G -v '$f':/etc/splash/proxy-profiles/ --net="bridge" scrapinghub/splash --maxrss '$u'; read x'
     sleep 0.1
     printf "$GREEN    Splash server launched on port $port_number$DEFAULT\n"
 done
