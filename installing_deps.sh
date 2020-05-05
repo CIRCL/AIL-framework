0d3f53ecd4a8066a8721c27a0fe219b55ece7b69
Terrtia <or1994@hotmail.fr>
2020-05-05 10:10:11 +0200

    chg: [travis install] shallow clone. fetch tags

--- installing_deps.sh
+++ installing_deps.sh
@@ -108,6 +108,8 @@ $AIL_HOME/doc/generate_modules_data_flow_graph.sh
 
 # init update version
 pushd ${AIL_HOME}
+# shallow clone
+git fetch --tags
 git describe --abbrev=0 --tags | tr -d '\n' > ${AIL_HOME}/update/current_version
 echo "AIL current version:"
 git describe --abbrev=0 --tags
