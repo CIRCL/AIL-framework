96a30170e329fe1690688f4b03fd6d92bb378595
lpwm9803 <olivier.sagit@orange.com>
2021-04-02 09:52:05 +0200

    feat: module factorization

--- bin/Indexer.py
+++ bin/Indexer.py
@@ -9,131 +9,148 @@ The ZMQ_Sub_Indexer modules is fetching the list of files to be processed
 and index each file with a full-text indexer (Whoosh until now).
 
 """
+##################################
+# Import External packages
+##################################
 import time
-from packages import Paste
-from pubsublogger import publisher
-
-from whoosh.index import create_in, exists_in, open_dir
-from whoosh.fields import Schema, TEXT, ID
 import shutil
 import os
 from os.path import join, getsize
+from whoosh.index import create_in, exists_in, open_dir
+from whoosh.fields import Schema, TEXT, ID
 
+
+##################################
+# Import Project packages
+##################################
+from module.abstract_module import AbstractModule
+from packages import Paste
 from Helper import Process
 
-# Config variable
-TIME_WAIT = 60*15 #sec
 
-# return in bytes
-def check_index_size(baseindexpath, indexname):
-    the_index_name = join(baseindexpath, indexname)
-    cur_sum = 0
-    for root, dirs, files in os.walk(the_index_name):
-        cur_sum += sum(getsize(join(root, name)) for name in files)
-    return cur_sum
+class Indexer(AbstractModule):
+    """
+    Indexer module for AIL framework
+    """
 
-def move_index_into_old_index_folder(baseindexpath):
-    for cur_file in os.listdir(baseindexpath):
-        if not cur_file == "old_index":
-            shutil.move(join(baseindexpath, cur_file), join(join(baseindexpath, "old_index"), cur_file))
+    # Time to wait in seconds between two index's size variable compute
+    TIME_WAIT = 60*15  # sec
+
+    def __init__(self):
+        """
+        Init Instance
+        """
+        super(Indexer, self).__init__()
+
+        # Indexer configuration - index dir and schema setup
+        self.baseindexpath = join(os.environ['AIL_HOME'],
+                                  self.process.config.get("Indexer", "path"))
+        self.indexRegister_path = join(os.environ['AIL_HOME'],
+                                       self.process.config.get("Indexer", "register"))
+        self.indexertype = self.process.config.get("Indexer", "type")
+        self.INDEX_SIZE_THRESHOLD = self.process.config.getint(
+            "Indexer", "index_max_size")
+
+        self.indexname = None
+        self.schema = None
+        self.ix = None
+
+        if self.indexertype == "whoosh":
+            self.schema = Schema(title=TEXT(stored=True), path=ID(stored=True,
+                                                             unique=True),
+                            content=TEXT)
+            if not os.path.exists(self.baseindexpath):
+                os.mkdir(self.baseindexpath)
+
+            # create the index register if not present
+            time_now = int(time.time())
+            if not os.path.isfile(self.indexRegister_path):  # index are not organised
+                self.redis_logger.debug("Indexes are not organized")
+                self.redis_logger.debug(
+                    "moving all files in folder 'old_index' ")
+                # move all files to old_index folder
+                self.move_index_into_old_index_folder()
+                self.redis_logger.debug("Creating new index")
+                # create all_index.txt
+                with open(self.indexRegister_path, 'w') as f:
+                    f.write(str(time_now))
+                # create dir
+                os.mkdir(join(self.baseindexpath, str(time_now)))
+
+            with open(self.indexRegister_path, "r") as f:
+                allIndex = f.read()
+                allIndex = allIndex.split()  # format [time1\ntime2]
+                allIndex.sort()
+
+                try:
+                    self.indexname = allIndex[-1].strip('\n\r')
+                except IndexError as e:
+                    self.indexname = time_now
+
+                self.indexpath = join(self.baseindexpath, str(self.indexname))
+                if not exists_in(self.indexpath):
+                    self.ix = create_in(self.indexpath, self.schema)
+                else:
+                    self.ix = open_dir(self.indexpath)
+
+            self.last_refresh = time_now
 
 
-if __name__ == "__main__":
-    publisher.port = 6380
-    publisher.channel = "Script"
-
-    config_section = 'Indexer'
-
-    p = Process(config_section)
-
-    # Indexer configuration - index dir and schema setup
-    baseindexpath = join(os.environ['AIL_HOME'],
-                             p.config.get("Indexer", "path"))
-    indexRegister_path = join(os.environ['AIL_HOME'],
-                             p.config.get("Indexer", "register"))
-    indexertype = p.config.get("Indexer", "type")
-    INDEX_SIZE_THRESHOLD = int(p.config.get("Indexer", "index_max_size"))
-    if indexertype == "whoosh":
-        schema = Schema(title=TEXT(stored=True), path=ID(stored=True,
-                                                         unique=True),
-                        content=TEXT)
-        if not os.path.exists(baseindexpath):
-            os.mkdir(baseindexpath)
-
-        # create the index register if not present
-        time_now = int(time.time())
-        if not os.path.isfile(indexRegister_path): #index are not organised
-            print("Indexes are not organized")
-            print("moving all files in folder 'old_index' ")
-            #move all files to old_index folder
-            move_index_into_old_index_folder(baseindexpath)
-            print("Creating new index")
-            #create all_index.txt
-            with open(indexRegister_path, 'w') as f:
-                f.write(str(time_now))
-            #create dir
-            os.mkdir(join(baseindexpath, str(time_now)))
-
-        with open(indexRegister_path, "r") as f:
-            allIndex = f.read()
-            allIndex = allIndex.split() # format [time1\ntime2]
-            allIndex.sort()
-
-            try:
-                indexname = allIndex[-1].strip('\n\r')
-            except IndexError as e:
-                indexname = time_now
-
-            indexpath = join(baseindexpath, str(indexname))
-            if not exists_in(indexpath):
-                ix = create_in(indexpath, schema)
-            else:
-                ix = open_dir(indexpath)
-
-        last_refresh = time_now
-
-    # LOGGING #
-    publisher.info("ZMQ Indexer is Running")
-
-    while True:
+    def compute(self, message):
         try:
-            message = p.get_from_set()
-
-            if message is not None:
-                PST = Paste.Paste(message)
-            else:
-                publisher.debug("Script Indexer is idling 1s")
-                time.sleep(1)
-                continue
+            PST = Paste.Paste(message)
             docpath = message.split(" ", -1)[-1]
             paste = PST.get_p_content()
-            print("Indexing - " + indexname + " :", docpath)
+            self.redis_logger.debug("Indexing - " + self.indexname + " :", docpath)
 
-
-            #avoid calculating the index's size at each message
-            if( time.time() - last_refresh > TIME_WAIT):
-                last_refresh = time.time()
-                if check_index_size(baseindexpath, indexname) >= INDEX_SIZE_THRESHOLD*(1000*1000):
+            # Avoid calculating the index's size at each message
+            if(time.time() - self.last_refresh > self.TIME_WAIT):
+                self.last_refresh = time.time()
+                if self.check_index_size() >= self.INDEX_SIZE_THRESHOLD*(1000*1000):
                     timestamp = int(time.time())
-                    print("Creating new index", timestamp)
-                    indexpath = join(baseindexpath, str(timestamp))
-                    indexname = str(timestamp)
-                    #update all_index
-                    with open(indexRegister_path, "a") as f:
+                    self.redis_logger.debug("Creating new index", timestamp)
+                    self.indexpath = join(self.baseindexpath, str(timestamp))
+                    self.indexname = str(timestamp)
+                    # update all_index
+                    with open(self.indexRegister_path, "a") as f:
                         f.write('\n'+str(timestamp))
-                    #create new dir
-                    os.mkdir(indexpath)
-                    ix = create_in(indexpath, schema)
+                    # create new dir
+                    os.mkdir(self.indexpath)
+                    self.ix = create_in(self.indexpath, self.schema)
 
-
-            if indexertype == "whoosh":
-                indexwriter = ix.writer()
+            if self.indexertype == "whoosh":
+                indexwriter = self.ix.writer()
                 indexwriter.update_document(
                     title=docpath,
                     path=docpath,
                     content=paste)
                 indexwriter.commit()
+
         except IOError:
-            print("CRC Checksum Failed on :", PST.p_path)
-            publisher.error('Duplicate;{};{};{};CRC Checksum Failed'.format(
+            self.redis_logger.debug("CRC Checksum Failed on :", PST.p_path)
+            self.redis_logger.error('Duplicate;{};{};{};CRC Checksum Failed'.format(
                 PST.p_source, PST.p_date, PST.p_name))
+
+    def check_index_size(self):
+        """
+        return in bytes
+        """
+        the_index_name = join(self.baseindexpath, self.indexname)
+        cur_sum = 0
+        for root, dirs, files in os.walk(the_index_name):
+            cur_sum += sum(getsize(join(root, name)) for name in files)
+
+        return cur_sum
+
+
+    def move_index_into_old_index_folder(self):
+        for cur_file in os.listdir(self.baseindexpath):
+            if not cur_file == "old_index":
+                shutil.move(join(self.baseindexpath, cur_file), join(
+                    join(self.baseindexpath, "old_index"), cur_file))
+
+
+if __name__ == '__main__':
+
+    module = Indexer()
+    module.run()
--- bin/ModuleStats.py
+++ bin/ModuleStats.py
@@ -5,151 +5,157 @@
 
 """
 
+##################################
+# Import External packages
+##################################
 import time
 import datetime
 import redis
 import os
+
+
+##################################
+# Import Project packages
+##################################
+from module.abstract_module import AbstractModule
 from packages.Date import Date
 from pubsublogger import publisher
 from Helper import Process
 from packages import Paste
-
-# Config Var
-max_set_cardinality = 8
-
-def get_date_range(num_day):
-    curr_date = datetime.date.today()
-    date = Date(str(curr_date.year)+str(curr_date.month).zfill(2)+str(curr_date.day).zfill(2))
-    date_list = []
-
-    for i in range(0, num_day+1):
-        date_list.append(date.substract_day(i))
-    return date_list
+import ConfigLoader
 
 
-def compute_most_posted(server, message):
-    module, num, keyword, paste_date = message.split(';')
+class ModuleStats(AbstractModule):
+    """
+    Module Statistics module for AIL framework
+    """
 
-    redis_progression_name_set = 'top_'+ module +'_set_' + paste_date
-
-    # Add/Update in Redis
-    server.hincrby(paste_date, module+'-'+keyword, int(num))
-
-    # Compute Most Posted
-    date = get_date_range(0)[0]
-    # check if this keyword is eligible for progression
-    keyword_total_sum = 0
-
-    curr_value = server.hget(date, module+'-'+keyword)
-    keyword_total_sum += int(curr_value) if curr_value is not None else 0
-
-    if server.zcard(redis_progression_name_set) < max_set_cardinality:
-        server.zadd(redis_progression_name_set, float(keyword_total_sum), keyword)
-
-    else: # not in set
-        member_set = server.zrangebyscore(redis_progression_name_set, '-inf', '+inf', withscores=True, start=0, num=1)
-        # Member set is a list of (value, score) pairs
-        if int(member_set[0][1]) < keyword_total_sum:
-            #remove min from set and add the new one
-            print(module + ': adding ' +keyword+ '(' +str(keyword_total_sum)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
-            server.zrem(redis_progression_name_set, member_set[0][0])
-            server.zadd(redis_progression_name_set, float(keyword_total_sum), keyword)
-            print(redis_progression_name_set)
+    # Config Var
+    MAX_SET_CARDINALITY = 8
 
 
-def compute_provider_info(server_trend, path):
-    redis_all_provider = 'all_provider_set'
+    def __init__(self):
 
-    paste = Paste.Paste(path)
+        super(ModuleStats, self).__init__()
 
-    paste_baseName = paste.p_name.split('.')[0]
-    paste_size = paste._get_p_size()
-    paste_provider = paste.p_source
-    paste_date = str(paste._get_p_date())
-    redis_sum_size_set = 'top_size_set_' + paste_date
-    redis_avg_size_name_set = 'top_avg_size_set_' + paste_date
-    redis_providers_name_set = 'providers_set_' + paste_date
+        # Waiting time in secondes between to message proccessed
+        self.pending_seconds = 20
 
-    # Add/Update in Redis
-    server_trend.sadd(redis_all_provider, paste_provider)
+        # Sent to the logging a description of the module
+        self.redis_logger.info("Makes statistics about valid URL")
 
-    num_paste = int(server_trend.hincrby(paste_provider+'_num', paste_date, 1))
-    sum_size = float(server_trend.hincrbyfloat(paste_provider+'_size', paste_date, paste_size))
-    new_avg = float(sum_size) / float(num_paste)
-    server_trend.hset(paste_provider +'_avg', paste_date, new_avg)
+        # REDIS #
+        self.r_serv_trend = ConfigLoader.ConfigLoader().get_redis_conn("ARDB_Trending")
+
+        self.r_serv_pasteName = ConfigLoader.ConfigLoader().get_redis_conn("Redis_Paste_Name")
 
 
-    #
-    # Compute Most Posted
-    #
+    def compute(self, message):
 
-    # Size
-    if server_trend.zcard(redis_sum_size_set) < max_set_cardinality or server_trend.zscore(redis_sum_size_set, paste_provider) != "nil":
-        server_trend.zadd(redis_sum_size_set, float(num_paste), paste_provider)
-        server_trend.zadd(redis_avg_size_name_set, float(new_avg), paste_provider)
-    else: #set full capacity
-        member_set = server_trend.zrangebyscore(redis_sum_size_set, '-inf', '+inf', withscores=True, start=0, num=1)
-        # Member set is a list of (value, score) pairs
-        if float(member_set[0][1]) < new_avg:
-            #remove min from set and add the new one
-            print('Size - adding ' +paste_provider+ '(' +str(new_avg)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
-            server_trend.zrem(redis_sum_size_set, member_set[0][0])
-            server_trend.zadd(redis_sum_size_set, float(sum_size), paste_provider)
-            server_trend.zrem(redis_avg_size_name_set, member_set[0][0])
-            server_trend.zadd(redis_avg_size_name_set, float(new_avg), paste_provider)
+        if len(message.split(';')) > 1:
+            self.compute_most_posted(message)
+        else:
+            self.compute_provider_info(message)
 
 
-    # Num
-    # if set not full or provider already present
-    if server_trend.zcard(redis_providers_name_set) < max_set_cardinality or server_trend.zscore(redis_providers_name_set, paste_provider) != "nil":
-        server_trend.zadd(redis_providers_name_set, float(num_paste), paste_provider)
-    else: #set at full capacity
-        member_set = server_trend.zrangebyscore(redis_providers_name_set, '-inf', '+inf', withscores=True, start=0, num=1)
-        # Member set is a list of (value, score) pairs
-        if int(member_set[0][1]) < num_paste:
-            #remove min from set and add the new one
-            print('Num - adding ' +paste_provider+ '(' +str(num_paste)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
-            server_trend.zrem(member_set[0][0])
-            server_trend.zadd(redis_providers_name_set, float(num_paste), paste_provider)
+    def get_date_range(self, num_day):
+        curr_date = datetime.date.today()
+        date = Date(str(curr_date.year)+str(curr_date.month).zfill(2)+str(curr_date.day).zfill(2))
+        date_list = []
+
+        for i in range(0, num_day+1):
+            date_list.append(date.substract_day(i))
+        return date_list
+
+
+    def compute_most_posted(self, message):
+        module, num, keyword, paste_date = message.split(';')
+
+        redis_progression_name_set = 'top_'+ module +'_set_' + paste_date
+
+        # Add/Update in Redis
+        self.r_serv_trend.hincrby(paste_date, module+'-'+keyword, int(num))
+
+        # Compute Most Posted
+        date = self.get_date_range(0)[0]
+        # check if this keyword is eligible for progression
+        keyword_total_sum = 0
+
+        curr_value = self.r_serv_trend.hget(date, module+'-'+keyword)
+        keyword_total_sum += int(curr_value) if curr_value is not None else 0
+
+        if self.r_serv_trend.zcard(redis_progression_name_set) < self.MAX_SET_CARDINALITY:
+            self.r_serv_trend.zadd(redis_progression_name_set, float(keyword_total_sum), keyword)
+
+        else: # not in set
+            member_set = self.r_serv_trend.zrangebyscore(redis_progression_name_set, '-inf', '+inf', withscores=True, start=0, num=1)
+            # Member set is a list of (value, score) pairs
+            if int(member_set[0][1]) < keyword_total_sum:
+                #remove min from set and add the new one
+                self.redis_logger.debug(module + ': adding ' +keyword+ '(' +str(keyword_total_sum)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
+                self.r_serv_trend.zrem(redis_progression_name_set, member_set[0][0])
+                self.r_serv_trend.zadd(redis_progression_name_set, float(keyword_total_sum), keyword)
+                self.redis_logger.debug(redis_progression_name_set)
+
+
+    def compute_provider_info(self, message):
+        redis_all_provider = 'all_provider_set'
+
+        paste = Paste.Paste(message)
+
+        paste_baseName = paste.p_name.split('.')[0]
+        paste_size = paste._get_p_size()
+        paste_provider = paste.p_source
+        paste_date = str(paste._get_p_date())
+        redis_sum_size_set = 'top_size_set_' + paste_date
+        redis_avg_size_name_set = 'top_avg_size_set_' + paste_date
+        redis_providers_name_set = 'providers_set_' + paste_date
+
+        # Add/Update in Redis
+        self.r_serv_pasteName.sadd(paste_baseName, message)
+        self.r_serv_trend.sadd(redis_all_provider, paste_provider)
+
+        num_paste = int(self.r_serv_trend.hincrby(paste_provider+'_num', paste_date, 1))
+        sum_size = float(self.r_serv_trend.hincrbyfloat(paste_provider+'_size', paste_date, paste_size))
+        new_avg = float(sum_size) / float(num_paste)
+        self.r_serv_trend.hset(paste_provider +'_avg', paste_date, new_avg)
+
+
+        #
+        # Compute Most Posted
+        #
+
+        # Size
+        if self.r_serv_trend.zcard(redis_sum_size_set) < self.MAX_SET_CARDINALITY or self.r_serv_trend.zscore(redis_sum_size_set, paste_provider) != "nil":
+            self.r_serv_trend.zadd(redis_sum_size_set, float(num_paste), paste_provider)
+            self.r_serv_trend.zadd(redis_avg_size_name_set, float(new_avg), paste_provider)
+        else: #set full capacity
+            member_set = self.r_serv_trend.zrangebyscore(redis_sum_size_set, '-inf', '+inf', withscores=True, start=0, num=1)
+            # Member set is a list of (value, score) pairs
+            if float(member_set[0][1]) < new_avg:
+                #remove min from set and add the new one
+                self.redis_logger.debug('Size - adding ' +paste_provider+ '(' +str(new_avg)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
+                self.r_serv_trend.zrem(redis_sum_size_set, member_set[0][0])
+                self.r_serv_trend.zadd(redis_sum_size_set, float(sum_size), paste_provider)
+                self.r_serv_trend.zrem(redis_avg_size_name_set, member_set[0][0])
+                self.r_serv_trend.zadd(redis_avg_size_name_set, float(new_avg), paste_provider)
+
+
+        # Num
+        # if set not full or provider already present
+        if self.r_serv_trend.zcard(redis_providers_name_set) < self.MAX_SET_CARDINALITY or self.r_serv_trend.zscore(redis_providers_name_set, paste_provider) != "nil":
+            self.r_serv_trend.zadd(redis_providers_name_set, float(num_paste), paste_provider)
+        else: #set at full capacity
+            member_set = self.r_serv_trend.zrangebyscore(redis_providers_name_set, '-inf', '+inf', withscores=True, start=0, num=1)
+            # Member set is a list of (value, score) pairs
+            if int(member_set[0][1]) < num_paste:
+                #remove min from set and add the new one
+                self.redis_logger.debug('Num - adding ' +paste_provider+ '(' +str(num_paste)+') in set and removing '+member_set[0][0]+'('+str(member_set[0][1])+')')
+                self.r_serv_trend.zrem(member_set[0][0])
+                self.r_serv_trend.zadd(redis_providers_name_set, float(num_paste), paste_provider)
 
 
 if __name__ == '__main__':
-    # If you wish to use an other port of channel, do not forget to run a subscriber accordingly (see launch_logs.sh)
-    # Port of the redis instance used by pubsublogger
-    publisher.port = 6380
-    # Script is the default channel used for the modules.
-    publisher.channel = 'Script'
 
-    # Section name in bin/packages/modules.cfg
-    config_section = 'ModuleStats'
-
-    # Setup the I/O queues
-    p = Process(config_section)
-
-    # Sent to the logging a description of the module
-    publisher.info("Makes statistics about valid URL")
-
-    # REDIS #
-    r_serv_trend = redis.StrictRedis(
-        host=p.config.get("ARDB_Trending", "host"),
-        port=p.config.get("ARDB_Trending", "port"),
-        db=p.config.get("ARDB_Trending", "db"),
-        decode_responses=True)
-
-    # Endless loop getting messages from the input queue
-    while True:
-        # Get one message from the input queue
-        message = p.get_from_set()
-
-        if message is None:
-            publisher.debug("{} queue is empty, waiting".format(config_section))
-            print('sleeping')
-            time.sleep(20)
-            continue
-
-        else:
-            # Do something with the message from the queue
-            if len(message.split(';')) > 1:
-                compute_most_posted(r_serv_trend, message)
-            else:
-                compute_provider_info(r_serv_trend, message)
+    module = ModuleStats()
+    module.run()
