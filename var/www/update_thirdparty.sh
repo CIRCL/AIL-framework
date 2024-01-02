b23683d98ba581b9ee35042790aee96e861ccfd9
terrtia <or1994@hotmail.fr>
2024-01-02 18:14:27 +0100

    chg: [thirdparties] remove sb-admin + debug #194

--- var/www/update_thirdparty.sh
+++ var/www/update_thirdparty.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 # submodules
 git submodule update
 
 wget -q http://dygraphs.com/dygraph-combined.js -O ./static/js/dygraph-combined.js
 
-SBADMIN_VERSION='3.3.7'
 BOOTSTRAP_VERSION='4.2.1'
 FONT_AWESOME_VERSION='5.7.1'
 D3_JS_VERSION='5.5.0'
@@ -15,20 +14,16 @@ D3_JS_VERSION='5.5.0'
 rm -rf temp
 mkdir temp
 
-wget -q https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip -O temp/bootstrap${BOOTSTRAP_VERSION}.zip
-wget -q https://github.com/BlackrockDigital/startbootstrap-sb-admin/archive/v${SBADMIN_VERSION}.zip -O temp/${SBADMIN_VERSION}.zip
-wget -q https://github.com/BlackrockDigital/startbootstrap-sb-admin-2/archive/v${SBADMIN_VERSION}.zip -O temp/${SBADMIN_VERSION}-2.zip
-wget -q https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip -O temp/FONT_AWESOME_4.7.0.zip
-wget -q https://github.com/FortAwesome/Font-Awesome/archive/5.7.1.zip -O temp/FONT_AWESOME_${FONT_AWESOME_VERSION}.zip
-wget -q https://github.com/d3/d3/releases/download/v${D3_JS_VERSION}/d3.zip -O  temp/d3_${D3_JS_VERSION}.zip
+wget https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip -O temp/bootstrap${BOOTSTRAP_VERSION}.zip
+wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip -O temp/FONT_AWESOME_4.7.0.zip
+wget https://github.com/FortAwesome/Font-Awesome/archive/5.7.1.zip -O temp/FONT_AWESOME_${FONT_AWESOME_VERSION}.zip
+wget https://github.com/d3/d3/releases/download/v${D3_JS_VERSION}/d3.zip -O  temp/d3_${D3_JS_VERSION}.zip
 
 # dateRangePicker
-wget -q https://github.com/moment/moment/archive/2.24.0.zip -O temp/moment.zip
-wget -q https://github.com/longbill/jquery-date-range-picker/archive/v0.20.0.zip -O temp/daterangepicker.zip
+wget https://github.com/moment/moment/archive/2.24.0.zip -O temp/moment.zip
+wget https://github.com/longbill/jquery-date-range-picker/archive/v0.20.0.zip -O temp/daterangepicker.zip
 
 unzip -qq temp/bootstrap${BOOTSTRAP_VERSION}.zip -d temp/
-unzip -qq temp/${SBADMIN_VERSION}.zip -d temp/
-unzip -qq temp/${SBADMIN_VERSION}-2.zip -d temp/
 unzip -qq temp/FONT_AWESOME_4.7.0.zip -d temp/
 unzip -qq temp/FONT_AWESOME_${FONT_AWESOME_VERSION}.zip -d temp/
 unzip -qq temp/d3_${D3_JS_VERSION}.zip -d temp/
@@ -41,8 +36,6 @@ mv temp/bootstrap-${BOOTSTRAP_VERSION}-dist/js/bootstrap.min.js.map ./static/js/
 mv temp/bootstrap-${BOOTSTRAP_VERSION}-dist/css/bootstrap.min.css ./static/css/bootstrap4.min.css
 mv temp/bootstrap-${BOOTSTRAP_VERSION}-dist/css/bootstrap.min.css.map ./static/css/bootstrap4.min.css.map
 
-mv temp/startbootstrap-sb-admin-${SBADMIN_VERSION} temp/sb-admin
-mv temp/startbootstrap-sb-admin-2-${SBADMIN_VERSION} temp/sb-admin-2
 mv temp/Font-Awesome-4.7.0 temp/font-awesome
 
 rm -rf ./static/webfonts/
@@ -50,15 +43,11 @@ mv temp/Font-Awesome-${FONT_AWESOME_VERSION}/css/all.min.css ./static/css/font-a
 mv temp/Font-Awesome-${FONT_AWESOME_VERSION}/webfonts ./static/webfonts
 
 rm -rf ./static/js/plugins
-mv temp/sb-admin/js/* ./static/js/
 
 rm -rf ./static/fonts/ ./static/font-awesome/
-mv temp/sb-admin/fonts/ ./static/
 mv temp/font-awesome/ ./static/
 
 rm -rf ./static/css/plugins/
-mv temp/sb-admin/css/* ./static/css/
-mv temp/sb-admin-2/dist/css/* ./static/css/
 mv temp/jquery-date-range-picker-0.20.0/dist/daterangepicker.min.css ./static/css/
 
 mv temp/d3.min.js ./static/js/
@@ -66,34 +55,35 @@ mv temp/moment-2.24.0/min/moment.min.js ./static/js/
 mv temp/jquery-date-range-picker-0.20.0/dist/jquery.daterangepicker.min.js ./static/js/
 
 JQVERSION="3.4.1"
-wget -q http://code.jquery.com/jquery-${JQVERSION}.js -O ./static/js/jquery.js
+wget http://code.jquery.com/jquery-${JQVERSION}.js -O ./static/js/jquery.js
 
 #Ressources for dataTable
-wget -q https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js -O ./static/js/jquery.dataTables.min.js
-wget -q https://cdn.datatables.net/plug-ins/1.10.20/integration/bootstrap/3/dataTables.bootstrap.css -O ./static/css/dataTables.bootstrap.css
-wget -q https://cdn.datatables.net/plug-ins/1.10.20/integration/bootstrap/3/dataTables.bootstrap.js -O ./static/js/dataTables.bootstrap.js
+wget https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js -O ./static/js/jquery.dataTables.min.js
+wget https://cdn.datatables.net/plug-ins/1.10.20/integration/bootstrap/3/dataTables.bootstrap.css -O ./static/css/dataTables.bootstrap.css
+wget https://cdn.datatables.net/plug-ins/1.10.20/integration/bootstrap/3/dataTables.bootstrap.js -O ./static/js/dataTables.bootstrap.js
 
-wget -q https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css -O ./static/css/dataTables.bootstrap.min.css
-wget -q https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js -O ./static/js/dataTables.bootstrap.min.js
+wget https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css -O ./static/css/dataTables.bootstrap.min.css
+wget https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js -O ./static/js/dataTables.bootstrap.min.js
 
 #Ressources for bootstrap popover
 POPPER_VERSION="1.16.1"
-wget -q https://github.com/FezVrasta/popper.js/archive/v${POPPER_VERSION}.zip -O temp/popper.zip
+wget https://github.com/FezVrasta/popper.js/archive/v${POPPER_VERSION}.zip -O temp/popper.zip
 unzip -qq temp/popper.zip -d temp/
 mv temp/floating-ui-${POPPER_VERSION}/dist/umd/popper.min.js ./static/js/
 mv temp/floating-ui-${POPPER_VERSION}/dist/umd/popper.min.js.map ./static/js/
 
 #Ressource for graph
-wget -q https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.js -O ./static/js/jquery.flot.js
-wget -q https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.pie.js -O ./static/js/jquery.flot.pie.js
-wget -q https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.time.js -O ./static/js/jquery.flot.time.js
-wget -q https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.stack.js -O ./static/js/jquery.flot.stack.js
+# DASHBOARD   # TODO REFACTOR DASHBOARD GRAPHS
+wget https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.js -O ./static/js/jquery.flot.js
+wget https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.pie.js -O ./static/js/jquery.flot.pie.js
+wget https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.time.js -O ./static/js/jquery.flot.time.js
+wget https://raw.githubusercontent.com/flot/flot/958e5fd43c6dff4bab3e1fd5cb6109df5c1e8003/jquery.flot.stack.js -O ./static/js/jquery.flot.stack.js
 
 #Ressources for sparkline and canvasJS and slider
-wget -q http://omnipotent.net/jquery.sparkline/2.1.2/jquery.sparkline.min.js -O ./static/js/jquery.sparkline.min.js
-wget -q https://canvasjs.com/assets/script/canvasjs.min.js -O ./static/js/jquery.canvasjs.min.js
+#wget http://omnipotent.net/jquery.sparkline/2.1.2/jquery.sparkline.min.js -O ./static/js/jquery.sparkline.min.js
+#wget https://canvasjs.com/assets/script/canvasjs.min.js -O ./static/js/jquery.canvasjs.min.js
 
-wget -q https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip -O temp/jquery-ui.zip
+wget https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip -O temp/jquery-ui.zip
 unzip -qq temp/jquery-ui.zip -d temp/
 mv temp/jquery-ui-1.12.1/jquery-ui.min.js ./static/js/jquery-ui.min.js
 mv temp/jquery-ui-1.12.1/jquery-ui.min.css ./static/css/jquery-ui.min.css
@@ -126,11 +116,11 @@ then
 fi
 
 #Update MISP Taxonomies and Galaxies
-pip3 install git+https://github.com/MISP/PyTaxonomies --upgrade
-pip3 install git+https://github.com/MISP/PyMISPGalaxies --upgrade
+pip3 install git+https://github.com/MISP/PyTaxonomies --upgrade  # TODO move to requirement
+pip3 install git+https://github.com/MISP/PyMISPGalaxies --upgrade  # TODO move to requirement
 
 #Update PyMISP
-pip3 install git+https://github.com/MISP/PyMISP --upgrade
+pip3 install pymisp --upgrade
 
 #Update the Hive
-pip3 install thehive4py --upgrade
+pip3 install thehive4py --upgrade  # TODO move to requirement
