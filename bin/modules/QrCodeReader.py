09260f68aec2ee45e622a352f10e8383b3c08119
terrtia <or1994@hotmail.fr>
2024-10-02 15:32:36 +0200

    fix: [qrcode extractor] debug

--- bin/modules/QrCodeReader.py
+++ bin/modules/QrCodeReader.py
@@ -54,6 +54,7 @@ class QrCodeReader(AbstractModule):
         try:
             image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
         except cv2.error:
+            print(self.obj.get_global_id())
             image = cv2.imread(path)
 
         try:
