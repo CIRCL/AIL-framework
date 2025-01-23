4007bd1633e52f0756fe68bd7c5ef6ca36f5507b
terrtia <or1994@hotmail.fr>
2025-01-23 10:26:03 +0100

    chg: [update v6.0.1] login lowercase

--- var/www/blueprints/root.py
+++ var/www/blueprints/root.py
@@ -70,7 +70,7 @@ def login():
             return render_template("login.html", error=logging_error)
 
     if request.method == 'POST':
-        username = request.form.get('username')
+        username = request.form.get('username', '').lower()
         password = request.form.get('password')
         next_page = request.form.get('next_page')
 
